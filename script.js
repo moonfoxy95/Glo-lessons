ae38d52147e5c7921de928e7d5acd7b69fc52b2f
vlad laptop <moonfoxy@icloud.com>
2026-03-13 20:44:41 +0300

    done lesson 2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
-alert('Alert message');
-console.log('Console message');
+let title = 'Project';
+let screens = 'Simple, Complex, Interactive';
+let screenPrice = 210;
+let rollback = 10;
+let fullPrice = 310;
+let adaptive = true;
+
+console.log(typeof (title), typeof (fullPrice), typeof (adaptive));
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей.\nСтоимость разработки сайта ${fullPrice} рублей.`)
+console.log(screens.toLocaleLowerCase().split(','));
+console.log(fullPrice * (rollback / 100));
